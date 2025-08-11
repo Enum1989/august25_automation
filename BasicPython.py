c89b343cb03e92c0e13c0d4907cd56d118ad5904
Vladimir Sokolyanskye <meloman1989@gmail.com>
2025-08-11 20:44:34 +0300

    Auto

---
+++ BasicPython.py
@@ -0,0 +1,36 @@
+my_number = 777
+print(my_number)
+
+a = 5
+b = 7
+print(a + b)
+result_a_b = a + b
+print(result_a_b)
+
+k = 42
+l = 57
+m = 83
+n = 14
+
+#x * 2 ** 3 / 2 + y
+print(k * 2 ** 3 / 2 + l)
+print(l * 2 ** 3 / 2 + m)
+print(m * 2 ** 3 / 2 + n)
+print(n * 2 ** 3 / 2 + k)
+
+def calculate(x, y):
+   print(x * 2 ** 3 / 2 + y)
+
+calculate(k, l)
+
+
+def work():
+   print('I am working')
+
+work()
+
+o = 7
+if o < 5:
+ print('Hello')
+else:
+ print('Bye')
---
+++ test_site.py
@@ -0,0 +1,27 @@
+import pytest
+import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+@pytest.fixture()
+def browser():
+ browser = webdriver.Firefox()
+ browser.maximize_window()
+ browser.implicitly_wait(3)
+ yield browser
+ browser.close()
+  
+def test_open_s6(browser):
+    browser.get('https://demoblaze.com')
+    galaxy_s6 = browser.find_element(By.XPATH, '//a[text()="Samsung galaxy s6"]')
+    galaxy_s6.click()
+    title = browser.find_element(By.CSS_SELECTOR, 'h2.name')
+    assert title.text == 'Samsung galaxy s6'
+
+def test_two_monitors(browser):
+        browser.get('https://demoblaze.com')
+        monitor_link = browser.find_element(By.CSS_SELECTOR, '''[onclick="byCat('monitor')"]''')
+        monitor_link.click()
+        time.sleep(2)
+        monitors = browser.find_elements(By.CSS_SELECTOR,'.card')
+        assert len(monitors) == 2
